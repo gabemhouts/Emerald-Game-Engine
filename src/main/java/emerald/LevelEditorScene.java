fc0db5c813c00493fe4ad51c07f0242604bae7c3
Gabe Houts <gabemhouts@gmail.com>
2021-05-04 09:55:01 -0500

    Emerald game engine version 0.02

--- src/main/java/emerald/LevelEditorScene.java
+++ src/main/java/emerald/LevelEditorScene.java
@@ -2,34 +2,64 @@ package emerald;
 
 import java.awt.event.KeyEvent;
 
+import static org.lwjgl.opengl.GL20.*;
+
 public class LevelEditorScene extends Scene {
+
+    private String vertexShaderSrc = "#version 330 core\n" +
+            "layout (location=0) in vec3 aPos;\n" +
+            "layout (location=1) in vec4 aColor;\n" +
+            "\n" +
+            "out vec4 fColor;\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "    fColor = aColor;\n" +
+            "    gl_Position = vec4(aPos, 1.0);\n" +
+            "}";
+
+    private  String fragmentShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "in vec4 fColor;\n" +
+            "\n" +
+            "out vec4 color;\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "    color = fColor;\n" +
+            "}";
+
+    private int vertexID, fragmentID, shaderProgram;
 
     private boolean changingScene = false;
     private float timeToChangeScenes = 2.0f;
 
     public LevelEditorScene() {
-        System.out.println("Inside Level Editor Scene");
+
+    }
+
+
+    public void init() {
+        // Compile and link shaders
+
+        // Load and compile vertex shader
+        vertexID = glCreateShader(GL_VERTEX_SHADER);
+        // Pass the shader to the GPU
+        glShaderSource(vertexID, vertexShaderSrc);
+        glCompileShader(vertexID);
+
+        // Check for errors in compilation
+        int success = glGetShaderi(vertexID, GL_COMPILE_STATUS);
+        if (success == GL_FALSE) {
+            int len = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH);
+            System.out.println("ERROR: 'defaultShader.glsl'\n\tVertex shader compilation failed");
+            System.out.println(glGetShaderInfoLog(vertexID, len));
+            assert false : "";
+        }
     }
 
     @Override
     public void update(float dt) {
 
-        System.out.println("" + (1 / dt) + "fps");
-
-        if (!changingScene && KeyListener.isKeyPressed(KeyEvent.VK_SPACE)) {
-            changingScene = true;
-        }
-
-        if (changingScene && timeToChangeScenes > 0) {
-            timeToChangeScenes -=dt;
-            Window.get().r -= dt * 5.0f;
-            Window.get().g -= dt * 5.0f;
-            Window.get().b -= dt * 5.0f;
-
-
-        }
-        else if (changingScene) {
-            Window.changeScene(1);
         }
     }
-}
